738687a1d82cc48daaf0d4af911994c0f715c6ba
cbrandt77 <84159639+cbrandt77@users.noreply.github.com>
2026-02-11 12:30:57 -0500

    Buildscript: move gradle.properties accessors to buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,24 +9,6 @@ plugins {
 	kotlin("jvm") version "2.2.20"
 }
 
-//region gradle.properties accessors
-// gradle.properties accessors, so we don't have to do e.g. `project.properties["mod_id"]` constantly
-val Project.mod_id: String by properties
-val Project.mod_name: String by properties
-val Project.mod_license: String by properties
-val Project.mod_version: String by properties
-val Project.mod_authors: String by properties
-val Project.mod_description: String by properties
-val Project.mod_group_id: String by properties
-val Project.parchment_mappings_version: String by properties
-val Project.parchment_minecraft_version: String by properties
-val Project.minecraft_version: String by properties
-val Project.minecraft_version_range: String by properties
-val Project.neo_version: String by properties
-val Project.neo_version_range: String by properties
-val Project.loader_version_range: String by properties
-//endregion
-
 version = mod_version
 group = mod_group_id
 
