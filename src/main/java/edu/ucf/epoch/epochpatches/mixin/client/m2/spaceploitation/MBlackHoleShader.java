875b8cd1345e55fe6b449401d32910eedd868c6c
cbrandt77 <84159639+cbrandt77@users.noreply.github.com>
2026-02-10 22:50:28 -0500

    MBlackHoleShader - only disable shader on Apple Silicon

--- src/main/java/edu/ucf/epoch/epochpatches/mixin/client/m2/spaceploitation/MBlackHoleShader.java
+++ src/main/java/edu/ucf/epoch/epochpatches/mixin/client/m2/spaceploitation/MBlackHoleShader.java
@@ -1,12 +1,16 @@
 package edu.ucf.epoch.epochpatches.mixin.client.m2.spaceploitation;
 
 import com.bawnorton.mixinsquared.TargetHandler;
+import edu.ucf.epoch.epochpatches.mixinsupport.MixinData;
 import net.minecraft.client.renderer.LevelRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * Black hole shader causes crash on Apple Silicon, so we disable it here.
+ */
 @SuppressWarnings({"InvalidMemberReference", "MixinAnnotationTarget"})
 @Mixin(LevelRenderer.class)
 public class MBlackHoleShader {
@@ -19,7 +23,8 @@ public class MBlackHoleShader {
 		at=@At("HEAD"),
 		cancellable = true
 	)
-	private void inject(CallbackInfo ci) {
-		ci.cancel();
+	private void disableOnAppleSilicon(CallbackInfo ci) {
+		if (MixinData.IS_APPLE_SILICON)
+			ci.cancel();
 	}
 }
--- src/main/java/edu/ucf/epoch/epochpatches/mixinsupport/MixinData.java
+++ src/main/java/edu/ucf/epoch/epochpatches/mixinsupport/MixinData.java
@@ -1,8 +1,24 @@
 package edu.ucf.epoch.epochpatches.mixinsupport;
 
+import oshi.PlatformEnum;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor.PhysicalProcessor;
+import oshi.software.os.OperatingSystem;
+
 public final class MixinData {
     public static Thread TERMINAL_THREAD = null;
-
-    private MixinData() {
+	
+	// copied from MacCentralProcessor#isArm
+	public static final boolean IS_APPLE_SILICON = SystemInfo.getCurrentPlatform() == PlatformEnum.MACOS
+       && new SystemInfo()
+	        .getHardware()
+	        .getProcessor()
+	        .getPhysicalProcessors()
+	        .stream()
+	        .map(PhysicalProcessor::getIdString)
+	        .anyMatch(id -> id.contains("arm"));
+	
+	
+	private MixinData() {
     }
 }
